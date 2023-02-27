bacf170511f4fbbd454636a85dbbc656f45b9545
ctrl-schaff <schaffjohnathan@gmail.com>
2023-02-26 23:03:44 -0800

    Add depth method add-on to compare to CourtPool

--- titanium_white/court_session.py
+++ titanium_white/court_session.py
@@ -2,6 +2,8 @@
 Collection of classes for implementating the wrapper around
 the courtlistener API
 """
+import json
+import math
 from loguru import logger
 import requests
 
@@ -82,6 +84,56 @@ class CourtSession:
         )
         response_instance = self.http_session.send(prepared_request_instance, **settings)
         return response_instance, prepared_request_instance
+
+    def get_query_depth(self, **kwargs):
+        """
+        Prepares the GET request and sends the query to the courtlistener API endpoint,
+        and then processes the response
+
+        Also adds an additional page_depth input which enables for further searching through the
+        results of an endpoint
+
+        Supports the same input arguments as the requests.Request method except the method
+        parameter, as this one is hard-coded for HTTP "GET"
+        |  :param url: URL to send.
+        |  :param headers: dictionary of headers to send.
+        |  :param files: dictionary of {filename: fileobject} files to multipart upload.
+        |  :param data: the body to attach to the request. If a dictionary or
+        |      list of tuples ``[(key, value)]`` is provided, form-encoding will
+        |      take place.
+        |  :param json: json for the body to attach to the request
+        |               (if files or data is not specified).
+        |  :param params: URL parameters to append to the URL. If a dictionary or
+        |      list of tuples ``[(key, value)]`` is provided, form-encoding will
+        |      take place.
+        |  :param auth: Auth handler or (user, pass) tuple.
+        |  :param cookies: dictionary or CookieJar of cookies to attach to this request.
+        |  :param hooks: dictionary of callback hooks, for internal usage.
+        """
+        page_depth = kwargs.pop("page_depth", 10)
+        query_results = {"count": 0}
+        logger.debug(f"Querying CourtListener:\n{json.dumps(kwargs, indent=4)}")
+        initial_response, __ = self.get_query(**kwargs)
+        json_body = initial_response.json()
+        query_results["count"] = json_body.get("count", 0)
+        num_query_pages = math.ceil(query_results["count"] / 20)
+        page_depth = min(page_depth, num_query_pages)
+
+        query_url = initial_response.url
+        logger.info(f"GET {query_url}")
+        query_results[query_url] = json_body.get("results", None)
+
+        for page_index in range(2, page_depth + 1):
+            kwargs["params"].update({"page": page_index})
+            logger.info(f"GET {kwargs.get('url', None)}")
+            query_response, __ = self.get_query(**kwargs)
+
+            query_results[query_response.url] = query_response.json()
+            query_url = initial_response.url
+            logger.info(f"GET {query_url}")
+            query_results[query_url] = json_body.get("results", None)
+
+        return query_results
 
     def __inject_authorization(self, query_args: dict) -> dict:
         """
